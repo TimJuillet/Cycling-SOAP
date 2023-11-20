893bd430762b83b509ac7311fd259b48705fbf79
Ozeliurs <ozeliurs@gmail.com>
2023-11-20 11:35:04 +0100

    fix: add right api_key

--- RoutingServer/OSMRoutingClient/OSMRoutingClient.cs
+++ RoutingServer/OSMRoutingClient/OSMRoutingClient.cs
@@ -12,7 +12,7 @@ namespace OSMRoutingClient
 {
     public class OSMRoutingClient
     {
-        private static string api_key = "5b3ce3597851110001cf6248d6b9b3b0c1e84c7d9b7b4b0b6a0b2a4d";
+        private static string api_key = "5b3ce3597851110001cf6248ba0ea999ab9e47e39f4ae0415f4840e3";
         private string base_url = "https://api.openrouteservice.org/v2/directions/cycling-road?api_key=" + api_key;
         static readonly HttpClient client = new HttpClient();
         
