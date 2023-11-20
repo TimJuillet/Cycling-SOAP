c946a06de64537e2840b609d2fdad040e7ba62ac
Timothee <astagios01@gmail.com>
2023-11-20 11:27:07 +0100

    fix: right imports

--- RoutingServer/OSMRoutingClient/OSMRoutingClient.cs
+++ RoutingServer/OSMRoutingClient/OSMRoutingClient.cs
@@ -1,8 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+using System.Text.Json;
+
 namespace OSMRoutingClient
 {
     public class OSMRoutingClient
     {
-        private string api_key = "5b3ce3597851110001cf6248d6b9b3b0c1e84c7d9b7b4b0b6a0b2a4d";
+        private static string api_key = "5b3ce3597851110001cf6248d6b9b3b0c1e84c7d9b7b4b0b6a0b2a4d";
         private string base_url = "https://api.openrouteservice.org/v2/directions/cycling-road?api_key=" + api_key;
         static readonly HttpClient client = new HttpClient();
         
