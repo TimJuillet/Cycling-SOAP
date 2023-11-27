8a272dacccf09f384f03bdf1aca3bbcb02d6b964
Timothee <astagios01@gmail.com>
2023-11-27 09:59:22 +0100

    feat: add static client

--- RoutingServer/RoutingServer/Service1.cs
+++ RoutingServer/RoutingServer/Service1.cs
@@ -12,9 +12,11 @@ namespace RoutingServer
     // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
     public class Service1 : IService1 { 
 
+        public static OSMRoutingClient.OSMRoutingClient client = new OSMRoutingClient.OSMRoutingClient();
+
         public Route GetTrajet(String start, String end)
         {
-            return new Route();
+            return client.getRoute(new Position(43.61570, 7.07136), new Position(43.58998, 7.12431));
         }
 
 
